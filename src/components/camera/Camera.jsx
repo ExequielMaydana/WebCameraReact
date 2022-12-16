7024b0c0e6679cb03c275b278ea186cd165cb6d1
Maydana Exequiel <maydanarkt@gmail.com>
2022-12-15 21:46:34 -0300

    probando

--- src/components/camera/Camera.jsx
+++ src/components/camera/Camera.jsx
@@ -1,80 +1,129 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './styles/styleCamera.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./styles/styleCamera.css";
 
 const Camera = () => {
-
   const videoRef = useRef(null);
   const photoRef = useRef(null);
 
   const [hasPhoto, setHasPhoto] = useState(false);
 
-  const [photo, setPhoto] = useState()
+  const [photoCapture, setPhotoCapture] = useState([]);
 
-   // la que va
-   const asd = () => {
-      let constraints = { video: {
+  const [esAndroid, setEsAndroid] = useState(false);
+
+  const [esIphone, setEsIphone] = useState(false);
+
+
+  // para detectar desde que dispositivo se esta viendo la aplicacion, IOS, Android, etc.
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/android/i.test(userAgent)) {
+    setEsAndroid(!esAndroid);
+  } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    setEsIphone(!esIphone);
+  }
+
+  // funcion que abre la camara
+  const getVideo = () => {
+    let constraints = {
+      video: {
         facingMode: "environment",
-        width: 500, height: 400
-      }  
-      } ;
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function (mediaStream) {
-          var video = videoRef.current;
-          video.srcObject = mediaStream;
-          video.onloadedmetadata = function (e) {
-            video.play();
-          };
-        })
-        .catch(function (err) {
-          console.log(err.name + ": " + err.message);
-        });
+        width: 500,
+        height: 400,
+      },
     };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (mediaStream) {
+        var video = videoRef.current;
+        video.srcObject = mediaStream;
+        const track = mediaStream.getVideoTracks()[0];
+        imageCapture = new ImageCapture(track);
+        video.onloadedmetadata = function (e) {
+          video.play();
+        };
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+      });
+  };
+
+    // probando que me devuelva un file
+    console.log(photoCapture);
+
+  let imageCapture;
+  const takePhoto = () => {
+    imageCapture
+      .takePhoto()
+      .then((blob) => {
+        //convierto lo que me trae el blob a un file, para procesarlo en el useDropzone
+        const file = new File([blob], "photofrentedni", {
+          type: blob.type,
+        });
+        // el blob es un objeto, similar a un archivo de datos PERO sin procesar.
+        // en blob estan los datos de la imagen cargada. Estos datos los guardo en un estado.
+        setPhotoCapture([file]);
+      })
+      .then((imageBitmap) => console.log(imageBitmap))
+      .catch((err) => console.log(err));
+
+    // esto es para que se muestre la imagen en la etiqueta canva.
+    const width = 500;
+    const height = 400;
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let context = photo.getContext("2d");
+    context.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  };
+
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let context = photo.getContext("2d");
+    context.clearRect(0, 0, photo.width, photo.height);
+    setHasPhoto(false);
+  };
 
 
   useEffect(() => {
-      asd();
+    getVideo();
   }, [videoRef]);
-
-  const takePhoto = () => {
-      const width = 500;
-      const height = 400;
-      let video = videoRef.current;
-      let photo = photoRef.current;
   
-      photo.width = width;
-      photo.height = height;
-  
-      let context = photo.getContext("2d");
-      context.drawImage(video, 0, 0, width, height);
-      setHasPhoto(true);
-    };
-  
-    const closePhoto = () => {
-      let photo = photoRef.current;
-      let context = photo.getContext("2d");
-      context.clearRect(0, 0, photo.width, photo.height);
-      setHasPhoto(false);
-    };
-
 
   return (
-    <section className='container-takeAPhoto'>
-    <div className='container-camera-canva'>
+    <section className="container-takeAPhoto">
+      <div className="container-camera-canva">
+        {esIphone ? (
+          
+          <div className="container-ios">
+                  {/* IOS */}
+            <h1>Soy un Iphone</h1>
+          </div>
+        ) : (
+          <div className="container-mobile">
+            {/* Android */}
+            <div className="camera">
+              <video ref={videoRef}></video>
+              <button onClick={takePhoto} className="bnt-capturar">
+                Capturar
+              </button>
+            </div>
 
-    <div className="camera">
-    <video ref={videoRef}></video>
-    <button onClick={takePhoto} className="bnt-capturar">Capturar</button>
-    </div>
+            <div className={"result" + (hasPhoto ? "hasPhoto" : "")}>
+              <canvas ref={photoRef}></canvas>
+              <button onClick={closePhoto} className="bnt-capturar">
+                Sacar otra foto
+              </button>
+            </div>
+          </div>
+        )}
+  
+      </div>
+    </section>
+  );
+};
 
-  <div className={"result" + (hasPhoto ? "hasPhoto" : "")}>
-    <canvas ref={photoRef}></canvas>
-    <button onClick={closePhoto} className="bnt-capturar">Sacar otra foto</button>
-  </div>
-
-    </div>
-</section>
-  )
-}
-
-export default Camera
+export default Camera;
